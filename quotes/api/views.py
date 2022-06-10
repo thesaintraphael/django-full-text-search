71cb72b15191fdead5cadcbf65b4030447b53886
Rafael Salimov <raphasalimov@gmail.com>
2022-06-10 22:08:50 +0400

    applied basic search

--- quotes/api/views.py
+++ quotes/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from django.db.models import Q
 
 
 from .serializers import QuoteSerializer
@@ -8,3 +9,16 @@ from ..models import Quote
 class QuoteListAPIView(ListAPIView):
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
+
+    def _apply_basic_search(self, qs):
+        """Searching whether the name or quote contains search term"""
+
+        search = self.request.query_params.get("search", "")
+
+        return qs.filter(
+            Q(name__icontains=search) |
+            Q(quote__icontains=search)
+        )
+
+    def get_queryset(self):
+        return self._apply_basic_search(super().get_queryset())
