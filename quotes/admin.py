f0f65af51be6cbc86befde8a5426dfe0dd5fb11f
Rafael Salimov <raphasalimov@gmail.com>
2022-06-10 20:31:53 +0400

    Added mangement command to create 10k quotes

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from quotes.models import Quote
+
+
+admin.site.register(Quote)
---
+++ quotes/management/commands/add_quotes.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from ...models import Quote
+
+
+class Command(BaseCommand):
+    help = "Adds quotes to the database"
+
+    def handle(self, *args, **options):
+        fake = Faker()
+
+        quotes_tuple = (Quote(name=fake.name(), quote=fake.text())
+                        for _ in range(10_000))
+
+        Quote.objects.bulk_create(quotes_tuple)
